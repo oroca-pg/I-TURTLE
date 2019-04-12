65678819bd611c979804a2c3942b939a5ba0ab4e
Denhanis <31359128+Denhanis@users.noreply.github.com>
2019-04-12 20:14:01 +0900

    Update setup.py

--- dust/setup.py
+++ dust/setup.py
@@ -1,6 +1,8 @@
+##You don't actually need it.
+
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-# fetch values from package.xml
-setup_args = generate_distutils_setup(packages=['dust'], package_dir={'': 'include'})
-setup(**setup_args)
+#from distutils.core import setup
+#from catkin_pkg.python_setup import generate_distutils_setup
+## fetch values from package.xml
+#setup_args = generate_distutils_setup(packages=['dust'], package_dir={'': 'include'})
+#setup(**setup_args)
